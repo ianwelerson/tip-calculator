0c326d1ef3131758573fec57d24da00f72f2931f
Ian Welerson <ian.nawes@gmail.com>
2021-08-13 18:23:40 -0300

    :white_check_mark: improve e2e tests

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -8,36 +8,25 @@
 // https://on.cypress.io/custom-commands
 // ***********************************************
 //
-//
-// -- This is a parent command --
-// Cypress.Commands.add('login', (email, password) => { ... })
-//
-//
-// -- This is a child command --
-// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
-//
-//
-// -- This is a dual command --
-// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
-//
-//
-// -- This will overwrite an existing command --
-// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
-
 Cypress.Commands.add('tipCalculation', ({ manual }) => {
   cy.get('#billValue input').type('100')
+  let tipAmount
+  let numberOfPersons
   
   if (manual) {
     cy.get('#customTip input').type('10')
-    cy.get('#numberOfPersons input').type('2')
-    
-    cy.get('#tipAmount').should('have.text', '5')
-    cy.get('#totalValue').should('have.text', '55')
-  } else {
-    cy.get('[name=tipPercent][value=50]').check({force: true})
-    cy.get('#numberOfPersons input').type('2')
 
-    cy.get('#tipAmount').should('have.text', '25')
-    cy.get('#totalValue').should('have.text', '75')
+    tipAmount = '5'
+    numberOfPersons = '55'
+  } else {
+    cy.get('[name=tipPercent][value=50]').check({ force: true })
+    
+    tipAmount = '25'
+    numberOfPersons = '75'
   }
+
+  cy.get('#numberOfPersons input').type('2')
+
+  cy.get('#tipAmount').should('have.text', tipAmount)
+  cy.get('#totalValue').should('have.text', numberOfPersons)
 })
